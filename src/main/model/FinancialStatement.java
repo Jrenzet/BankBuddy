6e0447f22eca459cb4f1dc43634dd5a2597f6408
Julian Renzetti <julian.renzetti@outlook.com>
2024-02-13 15:42:37 -0800

    Implementation + tests for FinancialStatement class

--- src/main/model/FinancialStatement.java
+++ src/main/model/FinancialStatement.java
@@ -4,15 +4,15 @@ package model;
 public class FinancialStatement {
 
     private int fiscalYear;
-    private int assets;
-    private int liabilities;
-    private int currentAssets;
-    private int currentLiabilities;
-    private int netIncome;
-    private int depreciationExpense;
-    private int interestExpense;
-    private int incomeTaxExpense;
-    private int debtRepaid;
+    private double assets;
+    private double liabil;
+    private double curAssets;
+    private double curLiabilities;
+    private double netInc;
+    private double depEx;
+    private double intEx;
+    private double taxEx;
+    private double debtRepaid;
 
     //REQUIRES: Year be a valid year in the format YYYY
     public FinancialStatement(int year) {
@@ -21,41 +21,77 @@ public class FinancialStatement {
 
     //REQUIRES: debtRepaid + interestExpense != 0
     //EFFECTS: calculates debt service coverage ratio (EBITDA / interestExpense + debtRepaid)
-    public double calculateDSC() {
-        return 0.0; //stub
+    public double calcDSC() {
+        return calcEbitda() / debtRepaid;
     }
 
     //EFFECTS: calculates net income left over after debt repayment
-    public int calculateFreeCashFlow() {
-        return 0; //stub
+    public double calcFreeCashFlow() {
+        return calcEbitda() - debtRepaid;
     }
 
     //REQUIRES: currentLiabilities != 0
     //EFFECTS: calculates ratio of current assets to current liabilities
-    public double calculateCurrentRatio() {
-        return 0.0; //stub
+    public double calcCurrentRatio() {
+        return curAssets / curLiabilities;
     }
 
     //EFFECTS: calculates Earnings Before Interest Tax and Depreciation/Amortization
-    public int calculateEbitda() {
-        return 0; //stub
+    public double calcEbitda() {
+        return netInc + depEx + intEx + taxEx;
     }
 
     //REQUIRES: assets - liabilities != 0
     //EFFECTS: calculates debt to equity ratio (equity = assets - liabilities)
-    public double calculateLeverage() {
-        return 0.0; //stub
+    public double calcLeverage() {
+        return calcEquity() / liabil;
     }
 
     //EFFECTS: calculates equity
-    public int calculateEquity() {
-        return 0; //stub
+    public double calcEquity() {
+        return assets - liabil;
     }
 
     //getters
 
     public int getFiscalYear() {
         return this.fiscalYear;
+    }
+
+    public double getAssets() {
+        return this.assets;
+    }
+
+    public double getLiabil() {
+        return this.liabil;
+    }
+
+    public double getCurAssets() {
+        return this.curAssets;
+    }
+
+    public double getCurLiabilities() {
+        return this.curLiabilities;
+    }
+
+    public double getNetInc() {
+        return this.netInc;
+    }
+
+    public double getDepEx() {
+        return this.depEx;
+    }
+
+    public double getIntEx() {
+        return this.intEx;
+    }
+
+    public double getTaxEx() {
+        return this.taxEx;
+    }
+
+    public double getDebtRepaid() {
+        return this.debtRepaid;
     }
 
     //setters
@@ -68,32 +104,32 @@ public class FinancialStatement {
         this.assets = assets;
     }
 
-    public void setLiabilities(int liabilities) {
-        this.liabilities = liabilities;
+    public void setLiabil(int liabil) {
+        this.liabil = liabil;
     }
 
-    public void setCurrentAssets(int currentAssets) {
-        this.currentAssets = currentAssets;
+    public void setCurAssets(int curAssets) {
+        this.curAssets = curAssets;
     }
 
-    public void setCurrentLiabilities(int currentLiabilities) {
-        this.currentLiabilities = currentLiabilities;
+    public void setCurLiabilities(int curLiabilities) {
+        this.curLiabilities = curLiabilities;
     }
 
-    public void setNetIncome(int netIncome) {
-        this.netIncome = netIncome;
+    public void setNetInc(int netInc) {
+        this.netInc = netInc;
     }
 
-    public void setDepreciationExpense(int depreciationExpense) {
-        this.depreciationExpense = depreciationExpense;
+    public void setDepEx(int depEx) {
+        this.depEx = depEx;
     }
 
-    public void setInterestExpense(int interestExpense) {
-        this.interestExpense = interestExpense;
+    public void setIntEx(int intEx) {
+        this.intEx = intEx;
     }
 
-    public void setIncomeTaxExpense(int incomeTaxExpense) {
-        this.incomeTaxExpense = incomeTaxExpense;
+    public void setTaxEx(int taxEx) {
+        this.taxEx = taxEx;
     }
 
     public void setDebtRepaid(int debtRepaid) {
--- src/test/model/FinancialStatementTest.java
+++ src/test/model/FinancialStatementTest.java
@@ -1,5 +1,59 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class FinancialStatementTest {
-    // delete or rename this class!
+
+    private FinancialStatement testStmt1;
+
+    @BeforeEach
+    public void setup() {
+        testStmt1 = new FinancialStatement(2024);
+        testStmt1.setAssets(50000);
+        testStmt1.setLiabil(25000);
+        testStmt1.setCurAssets(20000);
+        testStmt1.setCurLiabilities(10000);
+        testStmt1.setNetInc(10000);
+        testStmt1.setDepEx(5000);
+        testStmt1.setIntEx(1000);
+        testStmt1.setTaxEx(4000);
+        testStmt1.setDebtRepaid(15000);
+    }
+
+    @Test
+    public void testEbitda() {
+        double result = testStmt1.getNetInc() + testStmt1.getDepEx() + testStmt1.getIntEx() + testStmt1.getTaxEx();
+        assertEquals(result, testStmt1.calcEbitda());
+    }
+
+    @Test
+    public void testDSC() {
+        assertEquals(testStmt1.calcEbitda() / testStmt1.getDebtRepaid(), testStmt1.calcDSC());
+    }
+
+    @Test
+    public void testFreeCashFLow() {
+        double result = testStmt1.calcEbitda() - testStmt1.getDebtRepaid();
+        assertEquals(result, testStmt1.calcFreeCashFlow());
+    }
+
+    @Test
+    public void testCurrentRatio() {
+        assertEquals(testStmt1.getCurAssets() / testStmt1.getCurLiabilities(), testStmt1.calcCurrentRatio());
+    }
+
+    @Test
+    public void testLeverage() {
+        assertEquals(testStmt1.getLiabil() / testStmt1.calcEquity(), testStmt1.calcLeverage());
+    }
+
+    @Test
+    public void testEquity() {
+        assertEquals(testStmt1.getAssets() - testStmt1.getLiabil(), testStmt1.calcEquity());
+    }
+
+
 }
